ed99f2e700b56e7f8f08c28cc55e83181c5377b1
Tobias Mesquita <tobias.mesquita@gmail.com>
2022-07-16 20:36:11 -0300

    frontend

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </template>
 
 <script lang="ts">
+import { useQuasar } from 'quasar';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
+  setup () {
+    if (process.env.SERVER) {
+      const quasar = useQuasar()
+      quasar.dark.set(true)
+    }
+  }
 });
 </script>
