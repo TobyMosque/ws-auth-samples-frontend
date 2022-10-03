bcf4e975d0ff4403570e53f07a0cbb81bbf28ef1
Tobias Mesquita <tobias.mesquita@gmail.com>
2022-10-03 19:25:57 -0300

    refresh-before

--- quasar.config.js
+++ quasar.config.js
@@ -80,7 +80,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
     devServer: {
       server: {
-        type: 'https',
+        type: 'http',
       },
       port: 9103,
       open: false, // opens browser window automatically
